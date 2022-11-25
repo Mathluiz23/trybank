cd5c0500f401c5c6e39740eb1bdd6ce43fead704
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-11-25 17:42:47 -0300

    :sparkles: feat: Implementando função que irá registrar uma nova conta e verificar se a conta poderá ser criada ou se já está sendo usada

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -2,56 +2,66 @@
 
 public class Trybank
 {
-    public bool Logged;
-    public int loggedUser;
-    
-    //0 -> Número da conta
-    //1 -> Agência
-    //2 -> Senha
-    //3 -> Saldo
-    public int[,] Bank;
-    public int registeredAccounts;
-    private int maxAccounts = 50;
-    public Trybank()
-    {
-        loggedUser = -99;
-        registeredAccounts = 0;
-        Logged = false;
-        Bank = new int[maxAccounts, 4];
-    }
+  public bool Logged;
+  public int loggedUser;
 
-    public void RegisterAccount(int number, int agency, int pass)
-    {
-        throw new NotImplementedException();
-    }
+  //0 -> Número da conta
+  //1 -> Agência
+  //2 -> Senha
+  //3 -> Saldo
+  public int[,] Bank;
+  public int registeredAccounts;
+  private int maxAccounts = 50;
+  public Trybank()
+  {
+    loggedUser = -99;
+    registeredAccounts = 0;
+    Logged = false;
+    Bank = new int[maxAccounts, 4];
+  }
 
-    public void Login(int number, int agency, int pass)
+  public void RegisterAccount(int number, int agency, int pass)
+  {
+    for (int i = 0; i < registeredAccounts; i++)
     {
-        throw new NotImplementedException();
+      if (Bank[i, 0] == number && Bank[i, 1] == agency)
+      {
+        throw new ArgumentException("A conta já está sendo usada!");
+      }
     }
+    Bank[registeredAccounts, 0] = number;
+    Bank[registeredAccounts, 1] = agency;
+    Bank[registeredAccounts, 2] = pass;
+    registeredAccounts++;
+  }
 
-    public void Logout()
-    {
-        throw new NotImplementedException();
-    }
+  public void Login(int number, int agency, int pass)
+  {
+    throw new NotImplementedException();
+  }
 
-    public int CheckBalance()
-    {
-        throw new NotImplementedException();   
-    }
+  public void Logout()
+  {
+    throw new NotImplementedException();
+  }
 
-    public void Transfer(int destinationNumber, int destinationAgency, int value)
-    {
-        throw new NotImplementedException();
-    }
+  public int CheckBalance()
+  {
+    throw new NotImplementedException();
+  }
 
-    public void Deposit(int value)
-    {
-        throw new NotImplementedException();
-    }
+  public void Transfer(int destinationNumber, int destinationAgency, int value)
+  {
+    throw new NotImplementedException();
+  }
 
-    public void Withdraw(int value)
-    {
-        throw new NotImplementedException();
-    }
+  public void Deposit(int value)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Withdraw(int value)
+  {
+    throw new NotImplementedException();
+  }
 }
