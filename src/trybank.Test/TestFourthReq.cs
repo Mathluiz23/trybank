cd390f5da207be7b6fbf619ca9dda45241c763ff
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-11-25 19:38:02 -0300

    :white_check_mark: test(passing test): Implementando teste da função Transfer, irá verificar se é possível realizar uma transferência com sucesso

--- src/trybank.Test/TestFourthReq.cs
+++ src/trybank.Test/TestFourthReq.cs
@@ -7,24 +7,31 @@ namespace trybank.Test;
 
 public class TestFourthReq
 {
-    [Theory(DisplayName = "Deve transefir um valor com uma conta logada")]
-    [InlineData(0, 0)]
-    public void TestTransferSucess(int balance, int value)
-    {        
-        throw new NotImplementedException();
-    }
+  [Theory(DisplayName = "Deve transefir um valor com uma conta logada")]
+  [InlineData(0, 0)]
+  public void TestTransferSucess(int balance, int value)
+  {
+    var trybank = new Trybank();
+    trybank.RegisterAccount(0, 0, 0);
+    trybank.RegisterAccount(1, 1, 0);
+    trybank.Login(0, 0, 0);
+    trybank.Deposit(balance);
+    trybank.Transfer(1, 1, value);
 
-    [Theory(DisplayName = "Deve lançar uma exceção de usuário não logado")]
-    [InlineData(0)]
-    public void TestTransferWithoutLogin(int value)
-    {        
-        throw new NotImplementedException();
-    }
+    trybank.Bank[0, 3].Should().Be(balance - value);
+  }
 
-    [Theory(DisplayName = "Deve lançar uma exceção de usuário não logado")]
-    [InlineData(0, 0)]
-    public void TestTransferWithoutBalance(int balance, int value)
-    {        
-        throw new NotImplementedException();
-    }
+  [Theory(DisplayName = "Deve lançar uma exceção de usuário não logado")]
+  [InlineData(0)]
+  public void TestTransferWithoutLogin(int value)
+  {
+    throw new NotImplementedException();
+  }
+
+  [Theory(DisplayName = "Deve lançar uma exceção de usuário não logado")]
+  [InlineData(0, 0)]
+  public void TestTransferWithoutBalance(int balance, int value)
+  {
+    throw new NotImplementedException();
+  }
 }
